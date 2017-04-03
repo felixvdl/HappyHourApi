4da4eac8569c649d3a0c0dc964894d9f75d81644
Felix Vandelanotte <felixvandelanotte@Felixs-MacBook-Pro.local>
2017-04-03 14:23:05 -0400

    bug

--- app/controllers/api/v1/bars_controller.rb
+++ app/controllers/api/v1/bars_controller.rb
@@ -8,7 +8,7 @@ class Api::V1::BarsController < ApplicationController
     case Time.now.strftime("%A").downcase
     when "monday"
       bars = bars.select { |bar| bar.monday != "no"}
-      bars = bars.select { |bar| ((bar.monday.scan(/(\d{1,2}:?\d{0,2}|\D{1,2})/).first.first.to_i + 12)..(bar.monday.scan(/(\d{1,2}:?\d{0,2}|\D{1,2})/)[3].first.to_i + 12)).to_a.include?(hour.to_i) == true }
+      bars = bars.select { |bar| ((bar.monday.scan(/(\d{1,2}:?\d{0,2}|\D{1,2})/).first.first.to_i + 12)..(bar.monday.scan(/(\d{1,2}:?\d{0,2}|\D{1,2})/)[3].first.to_i + 12)).to_a.include?(hour.to_i) == false }
     when "wednesday"
       bars = bars.select { |bar| bar.wednesday != "no"}
       bars = bars.select { |bar| ((bar.wednesday.scan(/(\d{1,2}:?\d{0,2}|\D{1,2})/).first.first.to_i + 12)..(bar.wednesday.scan(/(\d{1,2}:?\d{0,2}|\D{1,2})/)[3].first.to_i + 12)).to_a.include?(hour.to_i) == true }
